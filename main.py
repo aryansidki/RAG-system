e6aa160d561527ccae588d62984b8b809b256011
Aryan Sidki <aryan.sidki@student.manchester.ac.uk>
2026-04-02 16:52:54 +0100

    Updated pdf reader to store page info as a list of dictionaries, preserving page info in a more organised way, rather than a list of strings. Now stores page info and text for each page.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pypdf import PdfReader
 
 def load_pdf_text(file_path):
     reader = PdfReader(file_path) #reader is an object representing the pdf, which we can work with
-    all_text = []
+    pages_data = []
 
     print(f"Number of pages: {len(reader.pages)}") #reader.pages gives access to the pages attribute of reader. allows us to access pages by index
 
@@ -10,12 +10,15 @@ def load_pdf_text(file_path):
         text = page.extract_text() 
 
         if text: #python treats 'truthy' non zero objects as true by default
-            all_text.append(text)
+            pages_data.append({"page_number": i+1, "text": text}) #dictionary stores properties of the page
         else:
             print(f"Warning: page {i+1} returned no extractable text")
     
-    return "\n".join(all_text) #uses newline as separator for each object in all_text
+    return pages_data
 
 pdf_path = "Transformerv3paper.pdf"
-text = load_pdf_text(pdf_path)
-print(text)
+pages = load_pdf_text(pdf_path)
+print(pages[1]["page_number"])
+print(pages[1]["text"])
+print(f"Pages with extracted text: {len(pages)}")
+
